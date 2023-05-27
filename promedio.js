9cd5f88930707a0d99c7a120d4e9e78181d0de7d
lucianoiv <lucianoarrietaingles@gmail.com>
2023-05-27 12:13:10 -0300

    Clase 27-5

--- promedio.js
+++ promedio.js
@@ -1,5 +1,20 @@
-let num1 = 5;
-let num2 = 7;
-let num3 = 9;
-let promedio = (num1+num2+num3) / 3;
-console.log(promedio);
+let num1 = 5.1;
+let num2 = 7.3;
+let num3 = 9.8;
+let num4 = 10.4;
+let num5 = 14.8;
+let num6 = 15.9;
+let promedio1 = (num1+num2+num3+num4+num5+num6) / 6;  //ejercicio promedio 6 clase practica sabado 27
+console.log("Promedio 1 es igual a " + promedio1);
+
+num1 = -2;
+num2 = -3; 
+num3 = -8;
+let promedio2 = (num1+num2+num3) / 3;
+console.log("Promedio 2 es igual a " + promedio2);
+
+num1 = -1.2; 
+num2 = 5.3; 
+num3 = -3.3;
+let promedio3 = (num1+num2+num3) / 3;
+console.log("Promedio 3 es igual a " + promedio3);
