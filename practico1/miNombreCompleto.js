9a8e908e45c15fa3b3db2f5e9de54a57f62e9d5e
lucianoiv <lucianoarrietaingles@gmail.com>
2023-06-13 14:45:29 -0300

    Practico1.1

--- practico1/miNombreCompleto.js
+++ practico1/miNombreCompleto.js
@@ -2,4 +2,4 @@ let nombre = "Luciano";
 let apellido = "Arrieta";
 let edad = "35";
 
-console.log("Hola, "+nombre+" "+apellido+". Tienes "+edad+" años.");
+console.log("Hola, %s %s. Tienes %i años.",nombre,apellido,edad);
